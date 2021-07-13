caf2e746fe12c2eec05e3017e54a1bd1011a8fd7
jgumerove <josh.gumerove@gmail.com>
2021-07-13 17:26:03 -0400

    create login path and partial to display login errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users
-
+  get "/login", to: "sessions#login", as: "login"
+  post "login", to: "sessions#create"
   delete "/logout", to: "sessions#logout", as: "logout"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
