a12a0cc47b66de8e50010d38e2773b5a27a80b88
jgumerove <josh.gumerove@gmail.com>
2021-07-14 11:14:36 -0400

    change path names for redirects

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,7 +19,7 @@ class SessionsController < ApplicationController
 
     def logout
         session.clear
-        redirect_to new_user_path
+        redirect_to signup_path
     end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id
-            redirect_to "http://localhost:3000"
+            redirect_to countries_path #may change this redirect / currently shows list of all the countries 
         else 
             render :new
         end
