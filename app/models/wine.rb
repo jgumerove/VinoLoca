cd6075ecbdb9fbb07f8b8f391f700e850c35b71f
jgumerove <josh.gumerove@gmail.com>
2021-07-19 10:45:14 -0400

    add validations to wine model

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,8 +1,10 @@
 class Wine < ApplicationRecord
   belongs_to :user
   belongs_to :country
-
   validates :title, :category, :year, :rating, :price, :country_id, presence: :true
+  validates :price, numericality: {greater_than_or_equal_to: 1}
+  validates :rating, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 100}
+  validates :year, numericality: {greater_than_or_equal_to: 1800, less_than_or_equal_to: Date.today.year}
   
   scope :alpha_wine, -> {order(:title)}
 
