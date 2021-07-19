bd6cffc9c707064748c1d4844cfc3cdf348f100b
jgumerove <josh.gumerove@gmail.com>
2021-07-19 11:01:40 -0400

    add separate_comma method to add comma to price if needed

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -8,4 +8,27 @@ class Wine < ApplicationRecord
   
   scope :alpha_wine, -> {order(:title)}
 
+    def separate_comma
+      a = self.price.to_s.split('')
+      b = a.size/3.0
+       if a.size < 4
+        self.price.to_s 
+      elsif a.size%3 == 0
+        n = -4
+       (b.to_i-1).times do |i|
+        a.insert(n, ',')
+        n -= 4
+    end
+       a.join("")
+    else
+        n = -4
+        b.to_i.times do |i|
+        a.insert(n, ',')
+        n -= 4
+    end
+        a.join("")
+    end
+  end
+
+
 end
